540a0f216f283db60cfeb7d2347367db8ebc72a6
Lucas David <lucascanellas@id.uff.br>
2023-12-16 23:00:35 -0300

    up

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import * as S from './styles';
+
+export const Home: React.FunctionComponent = () => {
+  return (
+    <S.Container>
+      <Text>Vasco</Text>
+    </S.Container>
+  );
+};
